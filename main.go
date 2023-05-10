16bc6bee42977957887e290de8a11aa09daec326
Rafael Rasso <rafael.rasso@gmail.com>
2023-05-10 20:30:12 -0300

    Perguntando se deseja continuar calculando

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 )
 import "bufio"
 
-// Garantir que uma operação válida seja informada v0.1.2
-// Perguntar se deseja continuar calculando ou parar de calcular v0.2.0
 // Registrar os resultados
 // Perguntar se deseja imprimir os ultimos resultados ao fechar o programa. v0.3.0
 // Transformação em arquivos. v0.4.0
@@ -18,67 +16,87 @@ func main() {
 	fmt.Println("Bem-vindos a magnifica calculadora feita em Golang!!!!!!!!!")
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var umValor float64
-	var err error
-	for {
-		fmt.Println("Informe um valor!")
-		if scanner.Scan() {
-			text := scanner.Text()
-			umValor, err = strconv.ParseFloat(text, 64)
-			if err == nil {
-				break
+	continuar := true
+
+	for continuar {
+		var umValor float64
+		var err error
+		for {
+			fmt.Println("Informe um valor!")
+			if scanner.Scan() {
+				text := scanner.Text()
+				umValor, err = strconv.ParseFloat(text, 64)
+				if err == nil {
+					break
+				}
 			}
+			fmt.Println("O valor informado é inválido")
 		}
-		fmt.Println("O valor informado é inválido")
-	}
 
-	var operacao int
-	for {
-		fmt.Println("Informe uma operação")
-		fmt.Println("1 - Somar")
-		fmt.Println("2 - Subtrair")
-		fmt.Println("3 - Multiplicar")
-		fmt.Println("4 - Dividr")
-		if scanner.Scan() {
-			text := scanner.Text()
-			operacao, err = strconv.Atoi(text)
-			if err == nil && operacao >= 1 && operacao <= 4 {
-				break
+		var operacao int
+		for {
+			fmt.Println("Informe uma operação")
+			fmt.Println("1 - Somar")
+			fmt.Println("2 - Subtrair")
+			fmt.Println("3 - Multiplicar")
+			fmt.Println("4 - Dividr")
+			if scanner.Scan() {
+				text := scanner.Text()
+				operacao, err = strconv.Atoi(text)
+				if err == nil && operacao >= 1 && operacao <= 4 {
+					break
+				}
 			}
+			fmt.Println("O valor informado é inválido")
 		}
-		fmt.Println("O valor informado é inválido")
-	}
 
-	var outroValor float64
-	for {
-		fmt.Println("Informe outro valor!")
-		if scanner.Scan() {
-			text := scanner.Text()
-			outroValor, err = strconv.ParseFloat(text, 64)
-			if err == nil {
-				break
+		var outroValor float64
+		for {
+			fmt.Println("Informe outro valor!")
+			if scanner.Scan() {
+				text := scanner.Text()
+				outroValor, err = strconv.ParseFloat(text, 64)
+				if err == nil {
+					break
+				}
 			}
+			fmt.Println("O valor informado é inválido")
 		}
-		fmt.Println("O valor informado é inválido")
+
+		var sinal string
+		var resultado float64
+		switch operacao {
+		case 1:
+			sinal = "+"
+			resultado = umValor + outroValor
+		case 2:
+			sinal = "-"
+			resultado = umValor - outroValor
+		case 3:
+			sinal = "*"
+			resultado = umValor * outroValor
+		case 4:
+			sinal = "/"
+			resultado = umValor / outroValor
+		}
+
+		fmt.Printf("O resultado de %.2f %s %.2f é igual a %.2f \n", umValor, sinal, outroValor, resultado)
+
+		for {
+			fmt.Println("Deseja continuar calculando? 1-Sim 2-Não")
+			if scanner.Scan() {
+
+				text := scanner.Text()
+				var textInt int
+				textInt, err = strconv.Atoi(text)
+				if err == nil && (textInt == 1 || textInt == 2) {
+					continuar = textInt == 1
+					break
+				}
+			}
+			fmt.Println("O valor informado é inválido")
+		}
+
 	}
-
-	var sinal string
-	var resultado float64
-	switch operacao {
-	case 1:
-		sinal = "+"
-		resultado = umValor + outroValor
-	case 2:
-		sinal = "-"
-		resultado = umValor - outroValor
-	case 3:
-		sinal = "*"
-		resultado = umValor * outroValor
-	case 4:
-		sinal = "/"
-		resultado = umValor / outroValor
-	}
-
-	fmt.Printf("O resultado de %.2f %s %.2f é igual a %.2f", umValor, sinal, outroValor, resultado)
-
+	fmt.Println("Bye bye!")
 }
